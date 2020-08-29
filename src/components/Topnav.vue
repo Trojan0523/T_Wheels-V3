5f271f61043b3205a041ac95e192346e0f1058a3
buxiongyu <buxiongyu@huya.com>
2020-08-29 16:45:32 +0800

    logo css添加

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -54,7 +54,16 @@ export default {
         }
     }
 
-    >.toggleAside {}
+    >.toggleAside {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        background: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
 
     @media (max-width: 500px) {
         >.menu {
