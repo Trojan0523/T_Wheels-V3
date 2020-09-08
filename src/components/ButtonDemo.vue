4b85645f17a72cb033212f9b2ab16e62ed4f76bd
buxiongyu <buxiongyu@huya.com>
2020-09-08 10:39:58 +0800

    基础样式添加，组件命名修改

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,13 +2,27 @@
   <div>Button 示例</div>
   <h1>示例</h1>
   <Button>你好</Button>
+  <Button theme="button">你好</Button>
+  <Button theme="link">你好</Button>
+  <Button theme="text">你好</Button>
+
 </template>
 
 <script lang="ts">
-import Button from '../lib/button.vue';
+import Button from '../lib/Button.vue';
+
 export default {
   components: {
     Button,
+  },
+  setup() {
+    const onClick = () => {
+      console.log('你好');
+    }
+    const onFocus = () => {
+      console.log('你好呀');
+    }
+    return {onClick, onFocus};
   }
 }
 </script>
