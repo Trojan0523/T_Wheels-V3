cb88c6d8ef6c0bc45367088d0c12ec56289b1413
buxiongyu <buxiongyu@huya.com>
2020-09-08 21:11:49 +0800

    创建Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,15 @@
 <template>
-
+  <div>Dialog 实例</div>
+  <h1>实例1</h1>
+<Dialog />
 </template>
 
 <script>
+import Dialog from '../lib/Dialog.vue';
 export default {
-
+  components: {
+    Dialog,
+  }
 }
 </script>
 
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <Button>取消</Button>
+      <Button>确定</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  name: 'Dialog',
+  components: {
+    Button,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
