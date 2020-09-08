f1e232fd8eb4f99b30da013bf2fc9e3ed1d397e4
buxiongyu <buxiongyu@huya.com>
2020-09-08 12:08:16 +0800

    添加size, level 属性， size css 2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,45 @@
 <template>
   <div>Button 示例</div>
-  <h1>示例</h1>
+  <h1>示例1</h1>
   <Button>你好</Button>
   <Button theme="button">你好</Button>
   <Button theme="link">你好</Button>
   <Button theme="text">你好</Button>
-
+  <div>Button 示例</div>
+  <h1>示例2</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
+  </div>
+  <div>
+    <Button theme="link" size="big">大大大</Button>
+    <Button theme="link">普普通</Button>
+    <Button size="small" theme="link">小小小</Button>
+  </div>
+  <div>
+    <Button size="big" theme="text">大大大</Button>
+    <Button theme="text">普普通</Button>
+    <Button size="small" theme="text">小小小</Button>
+  </div>
+  <h1>实例三</h1>
+  <div>
+    <div>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">主要文本按钮</Button>
+      <Button theme="text">普通文本按钮</Button>
+      <Button theme="text" level="danger">危险文本按钮</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,10 +1,12 @@
 <template>
-  <button class="trojan-button" :class="`trojan-theme-${theme}`">
+  <button class="trojan-button" :class="classes">
     <slot/>
   </button>
 </template>
 
 <script>
+import {computed} from 'vue';
+
 export default {
   name: 'Button',
   props: {
@@ -12,8 +14,26 @@ export default {
       type: String,
       default: 'button',
     },
-
-  }
+    size: {
+      type: String,
+      default: 'normal',
+    },
+    level: {
+      type: String,
+      default: 'normal'
+    },
+  },
+  setup(props) {
+    const {theme, size, level} = props;
+    const classes = computed(() => {
+      return {
+        [`trojan-theme-${theme}`]: theme,
+        [`trojan-size-${size}`]: size,
+        [`trojan-level-${level}`]: level,
+      };
+    });
+    return {classes};
+  },
 };
 </script>
 
@@ -22,6 +42,7 @@ $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
+$red: red;
 $radius: 4px;
 .trojan-button {
   box-sizing: border-box;
@@ -37,6 +58,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
 
   & + & {
     margin-left: 8px;
@@ -54,6 +76,89 @@ $radius: 4px;
 
   &::-moz-focus-inner {
     border: 0;
+  }
+
+  &.trojan-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.trojan-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+
+    &:hover, &:focus {
+      background: darken(white, 5%);
+    }
+  }
+
+  &.trojan-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+
+  &.trojan-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+
+  &.trojan-theme-button {
+    &.trojan-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+
+      &:hover, &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+
+    &.trojan-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+
+      &:hover, &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.trojan-theme-link {
+    &.trojan-lever-danger {
+      color: $red;
+
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+
+  &trojan-theme-text {
+    &.trojan-level-main {
+      color: $blue;
+
+      &:hover, &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+
+    &.trojan-level-danger {
+      color: $red;
+      &:hover, &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
