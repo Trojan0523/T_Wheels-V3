3870deddb6325d49d27416a997add6fb11fab5d5
buxiongyu <buxiongyu@huya.com>
2020-09-12 16:50:22 +0800

    Tabs css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index"/>
+  <div class="trojan-tabs">
+    <div class="trojan-tabs-nav">
+    <div class="trojan-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="trojan-tabs-content">
+      <component class="trojan-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index"/>
+    </div>
   </div>
 </template>
 
@@ -25,4 +29,29 @@ import Tab from './Tab.vue';
   }
 </script>
 
-<style></style>
+<style lang="scss">
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+  .trojan-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
+        &-item {
+          padding: 8px 0;
+          margin: 0 16px;
+          cursor: pointer;
+            &:first-child {
+              margin-left: 0;
+            }
+            &.selected {
+              color: $blue;
+            }
+        }
+    }
+    &-content {
+      padding: 8px 0;
+    }
+  }
+</style>
