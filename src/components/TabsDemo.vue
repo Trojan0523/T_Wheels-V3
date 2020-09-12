e7aa62812a3253ddeb8fc2a60ccc67443ce4859d
buxiongyu <buxiongyu@huya.com>
2020-09-12 17:28:50 +0800

    遇到Vue3 bug

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,19 +1,25 @@
 <template>
-<div>Tabs 示例</div>
+  <div>Tabs 示例</div>
   <h1>实例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航一">内容一</Tab>
     <Tab title="导航二">内容二</Tab>
   </Tabs>
 </template>
 
 <script lang="ts">
+import {ref} from 'vue';
 import Tabs from '../lib/Tabs.vue';
 import Tab from '../lib/Tab.vue';
+
 export default {
   components: {
     Tabs,
     Tab
+  },
+  setup() {
+    const x = ref('导航1');
+    return {x}
   }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,57 +1,79 @@
 <template>
   <div class="trojan-tabs">
     <div class="trojan-tabs-nav">
-    <div class="trojan-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{t}}</div>
+      <div class="trojan-tabs-nav-item"
+           @click="select(t)"
+           :class="{selected: t === selected}"
+           v-for="(t, index) in titles" :key="index">{{ t }}
+      </div>
     </div>
     <div class="trojan-tabs-content">
-      <component class="trojan-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index"/>
+      <component class="trojan-tabs-content-item" :is="current"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue';
-  export default {
-    setup(props, context) {
-      // console.log({...context.slots.default()});
-      const defaults = context.slots.default();
-      defaults.forEach((tag) => {
-        if(tag.type !== Tab) {
-          throw new Error('Tabs 字标签必须为Tab')
-        }
-      })
-      const titles =  defaults.map((tag) => {
-            return tag.props.title
-      })
-      console.log(defaults[0].type);
-      return {defaults, titles};
+import {computed} from 'vue';
+
+export default {
+  props: {
+    selected: {
+      type: String,
     }
+  },
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 字标签必须为Tab')
+      }
+    })
+    const select = (title: string) => {
+      context.emit('update:selected', title);
+    }
+    const current = computed(() => {
+      console.log('重新return');
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+      })[0]
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    return {defaults, titles, current, select};
   }
+}
 </script>
 
 <style lang="scss">
-  $blue: #40a9ff;
-  $color: #333;
-  $border-color: #d9d9d9;
-  .trojan-tabs {
-    &-nav {
-      display: flex;
-      color: $color;
-      border-bottom: 1px solid $border-color;
-        &-item {
-          padding: 8px 0;
-          margin: 0 16px;
-          cursor: pointer;
-            &:first-child {
-              margin-left: 0;
-            }
-            &.selected {
-              color: $blue;
-            }
-        }
-    }
-    &-content {
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.trojan-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
       padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &.selected {
+        color: $blue;
+      }
     }
   }
+
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
