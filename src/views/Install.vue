4de9fea5539ec6cf696a9c589506221dfe1b56f3
buxiongyu <buxiongyu@huya.com>
2020-09-19 10:44:44 +0800

    引入github样式

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,5 +1,15 @@
 <template>
-  安装
+  <article class="markdown-body">
+    <h1>安装</h1>
+    <p>打开终端运行下列命令：</p>
+    <pre><code>npm install king-ui</code></pre>
+    <p>或</p>
+    <pre><code>yarn add king-ui</code></pre>
+
+    <p>
+      下一节：<a href="#/doc/get-started">开始使用</a>
+    </p>
+  </article>
 </template>
 
 <script lang="ts"></script>
