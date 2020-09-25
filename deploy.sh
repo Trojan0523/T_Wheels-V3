f8f946afb9e0321d87388430e37590df08cfdf01
buxiongyu <buxiongyu@huya.com>
2020-09-25 10:52:22 +0800

    添加一键部署脚本

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@github.com:Trojan0523/Trojan-UI-website.git
+git push -f -u origin master &&
+cd -
