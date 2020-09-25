68472103c680baec8364d7abf138407708fbdd3e
buxiongyu <buxiongyu@huya.com>
2020-09-25 16:27:49 +0800

    修复Tabs 在Vue3 正式版 flush的breaking change

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -36,7 +36,7 @@ export default {
         const {left: left2} = selectedItem.value.getBoundingClientRect();
         const left = left2 - left1;
         indicator.value.style.left = left + 'px';
-      })
+      }, {flush: 'post'})
     });
     const current = computed(() => {
       return defaults.find(tag => tag.props.title === props.selected)
