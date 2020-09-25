fb9554b40b42889797661e955f43d61c6e4671f8
buxiongyu <buxiongyu@huya.com>
2020-09-25 16:59:36 +0800

    官网修改

--- src/router.ts
+++ src/router.ts
@@ -11,25 +11,28 @@ import getStarted from './markdown/get-started.md';
 
 import {h} from 'vue';
 import {createWebHashHistory, createRouter} from 'vue-router'
-const history = createWebHashHistory();
-const md = string => h(Markdown, {content: `../markdown/${string}.md`,key: string})
 
-export  const router = createRouter({
-    history,
-    routes: [
-        {path: '/', component: Home, },
-        {path: '/doc',component: Doc,
-          children: [
-            {path:"", redirect: '/doc/intro' },
-            {path: "intro", component: md(intro)},
-            {path: "install", component: md(install)},
-            {path: "get-started", component: md( getStarted)},
-            {path: "switch", component: SwitchDemo},
-            {path: "button", component: ButtonDemo},
-            {path: "dialog", component: DialogDemo},
-            {path: "tabs", component: TabsDemo},
-        ]},
-    ]
+const history = createWebHashHistory();
+const md = string => h(Markdown, {content: string, key: string})
+
+export const router = createRouter({
+  history,
+  routes: [
+    {path: '/', component: Home,},
+    {
+      path: '/doc', component: Doc,
+      children: [
+        {path: "", redirect: '/doc/intro'},
+        {path: "intro", component: md(intro)},
+        {path: "install", component: md(install)},
+        {path: "get-started", component: md(getStarted)},
+        {path: "switch", component: SwitchDemo},
+        {path: "button", component: ButtonDemo},
+        {path: "dialog", component: DialogDemo},
+        {path: "tabs", component: TabsDemo},
+      ]
+    },
+  ]
 })
 router.afterEach(() => {
 })
